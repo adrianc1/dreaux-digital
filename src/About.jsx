d3cd4252d8382aa80367ea6670011f55af3936df
adriansito <adriancrosby02@gmail.com>
2025-06-05 23:35:28 -0700

    rotatation added to pfp on scroll and click

--- src/About.jsx
+++ src/About.jsx
@@ -1,34 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import pfp from '../src/assets/pfp.png';
 
 function AboutUs() {
-	// useEffect(() => {
-	// 	const observer = new IntersectionObserver(
-	// 		(entries) => {
-	// 			entries.forEach((entry) => {
-	// 				if (entry.isIntersecting) {
-	// 					// entry.target.classList.add('fade-in-x');
-	// 					entry.target.classList.add('spin-pfp');
-	// 				} else {
-	// 					// entry.target.classList.remove('fade-in-x');
-	// 					entry.target.classList.remove('spin-pfp');
-	// 				}
-	// 			});
-	// 		},
-	// 		{ threshold: 0 }
-	// 	);
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						entry.target.classList.add('fade-in-x');
+						entry.target.classList.add('spin');
+					} else {
+						entry.target.classList.remove('fade-in-x');
+						entry.target.classList.remove('spin');
+					}
+				});
+			},
+			{ threshold: 0 }
+		);
 
-	// 	const pfp = document.querySelectorAll('.pfp');
-	// 	pfp.forEach((el) => observer.observe(el));
+		const pfp = document.querySelectorAll('.pfp');
+		pfp.forEach((el) => observer.observe(el));
 
-	// 	// Optional cleanup on unmount
-	// 	return () => {
-	// 		pfp.forEach((el) => observer.unobserve(el));
-	// 	};
-	// }, []);
+		// Optional cleanup on unmount
+		return () => {
+			pfp.forEach((el) => observer.unobserve(el));
+		};
+	}, []);
 
-	const rotateImg = (e) => {
-		e.target.classList.add('spin-pfp');
+	const [isSpinning, setIsSpinning] = useState(false);
+
+	const handleClick = () => {
+		setIsSpinning(true);
+
+		setTimeout(() => {
+			setIsSpinning(false);
+		}, 1000);
 	};
 	return (
 		<section id="about" className="bg-gray-300 text-gray-900 py-16 w-full">
@@ -39,8 +45,10 @@ function AboutUs() {
 				<img
 					src={pfp}
 					alt=""
-					className="pfp w-1/2 mx-auto mb-8  opacity-100 "
-					onPointerDown={rotateImg}
+					className={`pfp w-1/2 mx-auto mb-8  opacity-100 ${
+						isSpinning ? 'spin' : ''
+					}`}
+					onPointerDown={handleClick}
 				/>
 
 				<p className="mb-6 leading-relaxed">
