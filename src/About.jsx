abe820dce1273ea6688771e72c0328458cb6085d
adriansito <adriancrosby02@gmail.com>
2025-06-05 21:42:22 -0700

    added spin animation to profile photo

--- src/About.jsx
+++ src/About.jsx
@@ -2,35 +2,46 @@ import { useEffect } from 'react';
 import pfp from '../src/assets/pfp.png';
 
 function AboutUs() {
-	useEffect(() => {
-		const observer = new IntersectionObserver(
-			(entries) => {
-				entries.forEach((entry) => {
-					if (entry.isIntersecting) {
-						entry.target.classList.add('fade-in-x');
-					} else {
-						entry.target.classList.remove('fade-in-x');
-					}
-				});
-			},
-			{ threshold: 0 }
-		);
+	// useEffect(() => {
+	// 	const observer = new IntersectionObserver(
+	// 		(entries) => {
+	// 			entries.forEach((entry) => {
+	// 				if (entry.isIntersecting) {
+	// 					// entry.target.classList.add('fade-in-x');
+	// 					entry.target.classList.add('spin-pfp');
+	// 				} else {
+	// 					// entry.target.classList.remove('fade-in-x');
+	// 					entry.target.classList.remove('spin-pfp');
+	// 				}
+	// 			});
+	// 		},
+	// 		{ threshold: 0 }
+	// 	);
 
-		const pfp = document.querySelectorAll('.pfp');
-		pfp.forEach((el) => observer.observe(el));
+	// 	const pfp = document.querySelectorAll('.pfp');
+	// 	pfp.forEach((el) => observer.observe(el));
 
-		// Optional cleanup on unmount
-		return () => {
-			pfp.forEach((el) => observer.unobserve(el));
-		};
-	}, []);
+	// 	// Optional cleanup on unmount
+	// 	return () => {
+	// 		pfp.forEach((el) => observer.unobserve(el));
+	// 	};
+	// }, []);
+
+	const rotateImg = (e) => {
+		e.target.classList.add('spin-pfp');
+	};
 	return (
 		<section id="about" className="bg-gray-300 text-gray-900 py-16 w-full">
 			<div className="max-w-4xl mx-auto px-6">
 				<h2 className="text-4xl font-bold text-center mb-8">
 					ABOUT DREAUX DIGITAL
 				</h2>
-				<img src={pfp} alt="" className="pfp w-1/2 mx-auto mb-8  opacity-1 " />
+				<img
+					src={pfp}
+					alt=""
+					className="pfp w-1/2 mx-auto mb-8  opacity-100 "
+					onPointerDown={rotateImg}
+				/>
 
 				<p className="mb-6 leading-relaxed">
 					Hi, I’m Adrian — the developer behind Dreaux Digital. I help
