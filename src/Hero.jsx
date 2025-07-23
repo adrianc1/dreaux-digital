d001eb519f7283da41cfb8dedb89e17449a851db
adriansito <adriancrosby02@gmail.com>
2025-07-22 22:57:15 -0700

    hero background added and changed cta button to yellow

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,7 +2,15 @@ import { IoIosArrowDown } from 'react-icons/io';
 
 function Hero() {
 	return (
-		<div className="hero-container bg-black w-full h-screen text-center px-4 flex flex-col justify-center m-0 p-0 ">
+		<div
+			className="hero-container bg-black w-full h-screen text-center px-4 flex flex-col justify-center m-0 p-0"
+			style={{
+				backgroundImage:
+					'linear-gradient(rgba(0, 0, 0,0.8), rgba(0, 0, 0,0.5)), url(./src/assets/hero-bg.jpg)',
+				backgroundSize: 'cover',
+				backgroundRepeat: 'no-repeat',
+			}}
+		>
 			<div className="  hero-content h-[90%] flex flex-col justify-around lg:h-full">
 				<div className="pt-10 opacity-0 fade-in-element">
 					<h1 className=" title  dreaux-title text-[#FFD600] text-7xl font-bold tracking-wider md:text-9xl lg:pt-16">
@@ -14,18 +22,18 @@ function Hero() {
 				</div>
 
 				<div className="">
-					<h3 className="tagline text-white text-2xl mb-8 md:text-4xl">
+					<h3 className="tagline max-w-6xl mx-auto text-white text-2xl mb-8 md:text-4xl">
 						Web development and management, handling your online presence from
 						strategy to launch, so you can focus on growing your business.
 					</h3>
 				</div>
 				<a href="#services">
-					<button className="bg-[#7ddeff] px-4 text-lg font-bold py-2 rounded hover:bg-[#5CD8E8] transition w-45 mx-auto text-neutral-950 text-center md:w-52 md:py-4 cursor-pointer">
+					<button className="bg-[#FFD600] hover:bg-[#e6c100] text-black font-bold text-lg px-8 py-4 rounded-xl shadow-[0_0_20px_rgba(255,214,0,0.3)] hover:shadow-[0_0_30px_rgba(255,214,0,0.5)] transform hover:scale-105 transition-all duration-300 w-full max-w-xs mx-auto md:max-w-sm cursor-pointer">
 						View Services
 					</button>
 				</a>
 				<a href="#about">
-					<IoIosArrowDown className="text-[#FFD600] text-6xl mx-auto mt-4 animate-bounce" />
+					<IoIosArrowDown className="text-white text-6xl mx-auto mt-4 animate-bounce" />
 				</a>
 			</div>
 		</div>
