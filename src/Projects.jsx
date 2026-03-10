fbc2ff27087c67677b9d1eba2143cbb77a267bbd
adriansito <adriancrosby02@gmail.com>
2026-03-09 22:59:57 -0700

    changed order of projects and updated concrete sports app screenshot

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,5 @@
-import sportsAppImg from './assets/concretess.png';
+import concretess from './assets/concretess.png';
 import resume from './assets/ezresume.png';
-import coffeeShopImg from './assets/project3.png';
 import portfolioImg from './assets/project4.png';
 function Projects() {
 	return (
@@ -9,7 +8,7 @@ function Projects() {
 				<h2 className="text-4xl font-bold text-center mb-12">PROJECTS</h2>
 
 				<div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-					{/* Project Card 4 */}
+					{/* Project Card 1 */}
 					<div className="bg-gray-100 rounded-xl shadow overflow-hidden hover:shadow-lg transition">
 						<img
 							src={resume}
@@ -31,7 +30,33 @@ function Projects() {
 							</a>
 						</div>
 					</div>
-					{/* Project Card 1 */}
+
+					{/* Project Card 2 */}
+					<div className="bg-gray-100 rounded-xl shadow overflow-hidden hover:shadow-lg transition">
+						<img
+							src={concretess}
+							alt="Sports App Screenshot"
+							className="w-full h-56 object-cover object-top rounded-t-xl"
+						/>
+						<div className="p-4">
+							<h3 className="text-xl font-semibold mb-2">
+								Sports Progressive Web App
+							</h3>
+							<p className="text-sm text-gray-600 mb-3">
+								High School Sports PWA centralized web app for parents,
+								teachers, and students for team information.
+							</p>
+							<a
+								href="https://concretesports.app/"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="text-blue-500 hover:underline text-sm font-medium cursor-pointer"
+							>
+								View Project →
+							</a>
+						</div>
+					</div>
+					{/* Project Card 3 */}
 					<div className="bg-gray-100 rounded-xl shadow overflow-hidden hover:shadow-lg transition">
 						<img
 							src={portfolioImg}
@@ -55,57 +80,6 @@ function Projects() {
 							</a>
 						</div>
 					</div>
-
-					{/* Project Card 2 */}
-					<div className="bg-gray-100 rounded-xl shadow overflow-hidden hover:shadow-lg transition">
-						<img
-							src={sportsAppImg}
-							alt="Sports App Screenshot"
-							className="w-full h-56 object-cover object-top rounded-t-xl"
-						/>
-						<div className="p-4">
-							<h3 className="text-xl font-semibold mb-2">
-								Sports Progressive Web App
-							</h3>
-							<p className="text-sm text-gray-600 mb-3">
-								High School Sports PWA centralized web app for parents,
-								teachers, and students for team information.
-							</p>
-							<a
-								href="https://concretesports.app/"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="text-blue-500 hover:underline text-sm font-medium cursor-pointer"
-							>
-								View Project →
-							</a>
-						</div>
-					</div>
-
-					{/* Project Card 3 */}
-					{/* <div className="bg-gray-100 rounded-xl shadow overflow-hidden hover:shadow-lg transition">
-						<img
-							src={coffeeShopImg}
-							alt="Coffee Shop Screenshot"
-							className="w-full h-56 object-cover object-top rounded-t-xl"
-						/>
-						<div className="p-4">
-							<h3 className="text-xl font-semibold mb-2">
-								Cafe w/ Menu Website
-							</h3>
-							<p className="text-sm text-gray-600 mb-3">
-								A minimalist-feeling cafe website with a food and drink menu.
-							</p>
-							<a
-								href="https://olivascafe.netlify.app"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="text-blue-500 hover:underline text-sm font-medium cursor-pointer"
-							>
-								View Project →
-							</a>
-						</div>
-					</div> */}
 				</div>
 			</div>
 		</section>
